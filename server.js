20fba114f8d934030d09b59f38058a2f8e4c6cd9
GasparPeralta0 <gaspar_peralta09@hotmail.com>
2026-02-19 05:27:24 +0100

    Refactor bcrypt hash to synchronous method

--- server.js
+++ server.js
@@ -10,9 +10,8 @@ var fs = require("fs");
 var path = require("path");
 var bcrypt = require('bcrypt');
 
-bcrypt.hash("password", 12, function(err, hash) {
-  console.log(hash);
-});
+var hash = bcrypt.hashSync("password", 12);
+console.log(hash);
 
 app.use(function (req, res, next) {
   res.set({
