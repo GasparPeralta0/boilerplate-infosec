2f85831e55546d0cea2ca88800575a9fd14bb1c2
GasparPeralta0 <gaspar_peralta09@hotmail.com>
2026-02-19 16:13:57 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -54,7 +54,7 @@ const api = require('./server.js');
 
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-app.use('/api', api);
+app.use('/_api', api);
 
 app.get('/', function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
