06cb319406ba44633b37d89f862f1d60b15adb7a
GasparPeralta0 <gaspar_peralta09@hotmail.com>
2026-02-21 02:43:43 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -48,24 +48,18 @@ app.use(
 
 
 
-
 const api = require('./server.js');
 
 app.use(express.static('public'));
-app.disable('strict-transport-security');
 app.use('/_api', api);
 
-app.get('/server.js', function (req, res, next) {
-  fs.readFile(__dirname + '/server.js', function (err, data) {
-    if (err) return next(err);
-    res.type('txt').send(data.toString());
-  });
-});;
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
 
 let port = process.env.PORT || 3000;
-
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log('Your app is listening on port ' + port);
 });
 
 module.exports = app;
